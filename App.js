c4592c2791c384ade300146a13add07268a8ed74
craftsmon <craftsmon@outlook.com>
2018-10-04 09:42:34 +0300

    Added Images and configuration for app build

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
-import { Root } from "native-base";
-import { Font, AppLoading } from "expo";
-// import { Provider } from 'react-redux';
+import React, { Component } from 'react'
+import { Root } from 'native-base'
+import { Font, AppLoading } from 'expo'
+import { Provider } from 'react-redux'
 
 /**
  * Routes tag
  * 
  * Defines all screens in the main 'Stack'
  */
-import Routes from './src/Routes';
+import Routes from './src/Routes'
+import Store from './src/Store'
 
 export default class App extends Component {
   constructor(props) {
-    super(props);
-    this.state = { loading: true };
+    super(props)
+    this.state = { loading: true }
   }
 
   async componentWillMount() {
@@ -21,8 +22,8 @@ export default class App extends Component {
       Roboto: require("native-base/Fonts/Roboto.ttf"),
       Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
       
-    });
-    this.setState({ loading: false });
+    })
+    this.setState({ loading: false })
   }
 
   render() {
@@ -31,12 +32,14 @@ export default class App extends Component {
         <Root>
           <AppLoading />
         </Root>
-      );
+      )
     }
     return (
       <Root>
-        <Routes />
+        <Provider store={Store}>
+          <Routes />
+        </Provider>
       </Root>
-    );
+    )
   }
 }
